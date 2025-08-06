a30b963e67d4a66f5b1aa16709d400c9c81661eb
Guido <scorpsan@aol.com>
2025-08-06 15:17:44 -0400

    First commit for UI feature

--- SWE_App/Window.cpp
+++ SWE_App/Window.cpp
@@ -6,6 +6,6 @@ Window::Window() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), w
 	//button2 = new wxButton(this, wxID_ANY, "Good Bye", wxPoint(120, 10), wxSize(100, 100));
 	//textBox = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 120), wxSize(230, 25));
 
-	//Test to see if commit to dev goes to main branch
-	//Test 2 to see if commit only goes to dev branch
+	//Starting my feature branch. Will make buttons first. 
+
 }
