ac4e5fb2d0d128620617db071e5901899a2ddeb7
Guido <scorpsan@aol.com>
2025-08-06 14:39:37 -0400

    Dev test commit

--- SWE_App/Window.cpp
+++ SWE_App/Window.cpp
@@ -5,4 +5,6 @@ Window::Window() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), w
 	//button1 = new wxButton(this, wxID_ANY, "Hello", wxPoint(10, 10), wxSize(100, 100));
 	//button2 = new wxButton(this, wxID_ANY, "Good Bye", wxPoint(120, 10), wxSize(100, 100));
 	//textBox = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 120), wxSize(230, 25));
+
+	//Test to see if commit to dev goes to main branch
 }
