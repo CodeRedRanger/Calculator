6c71323b817fe08279f7ec58875450a9343e155d
Guido <scorpsan@aol.com>
2025-08-06 14:24:59 -0400

    Set up dev branch and basic window for calculator

---
+++ SWE_App/Window.cpp
@@ -0,0 +1,8 @@
+#include "Window.h"
+
+Window::Window() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), wxSize(500, 500))
+{
+	//button1 = new wxButton(this, wxID_ANY, "Hello", wxPoint(10, 10), wxSize(100, 100));
+	//button2 = new wxButton(this, wxID_ANY, "Good Bye", wxPoint(120, 10), wxSize(100, 100));
+	//textBox = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 120), wxSize(230, 25));
+}
---
+++ SWE_App/Window.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "wx/wx.h"
+
+class Window : public wxFrame
+{
+private:
+	//buttons here
+	//text control here
+	
+	//wxButton* button1 = nullptr;
+	//wxButton* button2 = nullptr;
+	//wxTextCtrl* textBox = nullptr;
+
+public:
+	Window(); //wxFrame requires a default constructor
+	
+};
+
