db44289795a6b7e370ab20e6f1eb3300fb4ae65b
Guido <scorpsan@aol.com>
2025-08-07 12:06:08 -0400

    Added all 23 buttons

--- SWE_App/Window.cpp
+++ SWE_App/Window.cpp
@@ -12,8 +12,33 @@ Window::Window() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), w
 	float buttonHeight = (windowHeight / 20.0f);
 
 	button1 = new wxButton(this, wxID_ANY, "sin", wxPoint((windowWidth/8), (windowHeight/10)*4), wxSize(buttonWidth, buttonHeight));  
-	button2 = new wxButton(this, wxID_ANY, "cos", wxPoint((windowWidth/16) *5, (windowHeight/10) * 4), wxSize(buttonWidth, buttonHeight));
+	button2 = new wxButton(this, wxID_ANY, "cos", wxPoint((windowWidth/16)*5, (windowHeight/10)*4), wxSize(buttonWidth, buttonHeight));
+	button3 = new wxButton(this, wxID_ANY, "tan", wxPoint((windowWidth/16)*8, (windowHeight/10)*4), wxSize(buttonWidth, buttonHeight));
 	
+	button4 = new wxButton(this, wxID_ANY, "clear", wxPoint((windowWidth/8), (windowHeight/20)*9), wxSize(buttonWidth, buttonHeight));
+	button5 = new wxButton(this, wxID_ANY, "+/-", wxPoint((windowWidth/16)*5, (windowHeight/20)*9), wxSize(buttonWidth, buttonHeight));
+	button6 = new wxButton(this, wxID_ANY, "mod(%)", wxPoint((windowWidth/16)*8, (windowHeight/20)*9), wxSize(buttonWidth, buttonHeight));
+	button7 = new wxButton(this, wxID_ANY, "/", wxPoint((windowWidth/16)*11, (windowHeight/20)*9), wxSize(buttonWidth, buttonHeight));
+	
+	button8 = new wxButton(this, wxID_ANY, "7", wxPoint((windowWidth/8), (windowHeight/20)*10), wxSize(buttonWidth, buttonHeight));
+	button9 = new wxButton(this, wxID_ANY, "8", wxPoint((windowWidth/16)*5, (windowHeight/20)*10), wxSize(buttonWidth, buttonHeight));
+	button10 = new wxButton(this, wxID_ANY, "9", wxPoint((windowWidth/16)*8, (windowHeight/20)*10), wxSize(buttonWidth, buttonHeight));
+	button11 = new wxButton(this, wxID_ANY, "*", wxPoint((windowWidth/16)*11, (windowHeight/20)*10), wxSize(buttonWidth, buttonHeight));
+	
+	button12 = new wxButton(this, wxID_ANY, "4", wxPoint((windowWidth/8), (windowHeight/20)*11), wxSize(buttonWidth, buttonHeight));
+	button13 = new wxButton(this, wxID_ANY, "5", wxPoint((windowWidth/16)*5, (windowHeight/20)*11), wxSize(buttonWidth, buttonHeight));
+	button14 = new wxButton(this, wxID_ANY, "6", wxPoint((windowWidth/16)*8, (windowHeight/20)*11), wxSize(buttonWidth, buttonHeight));
+	button15 = new wxButton(this, wxID_ANY, "-", wxPoint((windowWidth/16)*11, (windowHeight/20)*11), wxSize(buttonWidth, buttonHeight));
+	
+	button16 = new wxButton(this, wxID_ANY, "1", wxPoint((windowWidth/8), (windowHeight/20)*12), wxSize(buttonWidth, buttonHeight));
+	button17 = new wxButton(this, wxID_ANY, "2", wxPoint((windowWidth/16)*5, (windowHeight/20)*12), wxSize(buttonWidth, buttonHeight));
+	button18 = new wxButton(this, wxID_ANY, "3", wxPoint((windowWidth/16)*8, (windowHeight/20)*12), wxSize(buttonWidth, buttonHeight));
+	button19 = new wxButton(this, wxID_ANY, "+", wxPoint((windowWidth/16)*11, (windowHeight/20)*12), wxSize(buttonWidth, buttonHeight));
+	
+	button20 = new wxButton(this, wxID_ANY, "back/del", wxPoint((windowWidth/8), (windowHeight/20)*13), wxSize(buttonWidth, buttonHeight));
+	button21 = new wxButton(this, wxID_ANY, "0", wxPoint((windowWidth/16)*5, (windowHeight/20)*13), wxSize(buttonWidth, buttonHeight));
+	button22 = new wxButton(this, wxID_ANY, ".", wxPoint((windowWidth/16)*8, (windowHeight/20)*13), wxSize(buttonWidth, buttonHeight));
+	button23 = new wxButton(this, wxID_ANY, "=", wxPoint((windowWidth/16)*11, (windowHeight/20)*13), wxSize(buttonWidth, buttonHeight));
 		                              
 	//textBox = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 120), wxSize(230, 25));
 
--- SWE_App/Window.h
+++ SWE_App/Window.h
@@ -9,6 +9,29 @@ private:
 	
 	wxButton* button1 = nullptr;
 	wxButton* button2 = nullptr;
+	wxButton* button3 = nullptr;
+	wxButton* button4 = nullptr;
+	wxButton* button5 = nullptr;
+	wxButton* button6 = nullptr;
+	wxButton* button7 = nullptr;
+	wxButton* button8 = nullptr;
+	wxButton* button9 = nullptr;
+	wxButton* button10 = nullptr;
+	wxButton* button11 = nullptr;
+	wxButton* button12 = nullptr;
+	wxButton* button13 = nullptr;
+	wxButton* button14 = nullptr;	
+	wxButton* button15 = nullptr;
+	wxButton* button16 = nullptr;
+	wxButton* button17 = nullptr;	
+	wxButton* button18 = nullptr;
+	wxButton* button19 = nullptr;
+	wxButton* button20 = nullptr;
+	wxButton* button21 = nullptr;
+	wxButton* button22 = nullptr;
+	wxButton* button23 = nullptr;	
+
+
 	//wxTextCtrl* textBox = nullptr;
 
 public:
