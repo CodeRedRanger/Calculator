3ce289648da24bc2089410f72590551ccbf8ac70
Guido <scorpsan@aol.com>
2025-08-17 18:00:31 -0400

    Deleted stale button code in windows

--- SWE_App/Window.h
+++ SWE_App/Window.h
@@ -8,8 +8,9 @@
 
 class Window : public wxFrame
 {
-private:
+
 public:
+	//made public for access in ButtonFactory
 	wxTextCtrl* textBox = nullptr;
 
 private:
@@ -42,42 +43,8 @@ private:
 public:
 	Window(); //wxFrame requires a default constructor
 
-	//wxTextCtrl* GetTextBox()const { return textBox; }
-	//void SetTextBox(wxTextCtrl* newTextBox) { textBox = newTextBox; }
-
 	//Handles font size changes when window is resized	
 	void OnSizeChange(wxSizeEvent& event);
-
-	//numbers
-	//void OnClick0(wxCommandEvent& event); 
-	
-	//void OnClick1(wxCommandEvent& event); 
-	//void OnClick2(wxCommandEvent& event);
-	//void OnClick3(wxCommandEvent& event);
-	//void OnClick4(wxCommandEvent& event);
-	//void OnClick5(wxCommandEvent& event);
-	//void OnClick6(wxCommandEvent& event);
-	//void OnClick7(wxCommandEvent& event);
-	//void OnClick8(wxCommandEvent& event);
-	//void OnClick9(wxCommandEvent& event); 
-	
-
-	//math functions
-	//void OnClickAdd(wxCommandEvent& event);
-	//void OnClickSubt(wxCommandEvent& event);
-	//void OnClickMult(wxCommandEvent& event);
-	//void OnClickDiv(wxCommandEvent& event);
-	//void OnClickMod(wxCommandEvent& event);
-	//void OnClickNeg(wxCommandEvent& event);
-	//void OnClickDecimal(wxCommandEvent& event);
-	//void OnClickEquals(wxCommandEvent& event);
-	//void OnClickSin(wxCommandEvent& event);
-	//void OnClickCos(wxCommandEvent& event);
-	//void OnClickTan(wxCommandEvent& event);
-
-	//other functions
-	//void OnClickClear(wxCommandEvent& event);
-	//void OnClickDelete(wxCommandEvent& event); 
 
 
 	wxDECLARE_EVENT_TABLE();
